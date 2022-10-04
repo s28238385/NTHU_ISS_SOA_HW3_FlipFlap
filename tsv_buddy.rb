bfa118c23ca3d005237e511e47c1eca21a3bd996
KaiYi Chu <s28238385@gmail.com>
2022-10-04 14:28:33 +0800

    Clone homework file

---
+++ tsv_buddy.rb
@@ -0,0 +1,14 @@
+# Module that can be included (mixin) to take and output TSV data
+module TsvBuddy
+  # take_tsv: converts a String with TSV data into @data
+  # parameter: tsv - a String in TSV format
+  def take_tsv(tsv)
+
+  end
+
+  # to_tsv: converts @data into tsv string
+  # returns: String in TSV format
+  def to_tsv
+
+  end
+end
